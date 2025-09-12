71e82c8ece368c509ef4b9c70f99084450098055
Gowri Bhat <97337263+gowribhat@users.noreply.github.com>
2025-09-12 22:05:49 +0800

    Link cart to context

--- assignment2/src/components/Navbar.js
+++ assignment2/src/components/Navbar.js
@@ -1,12 +1,38 @@
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-    return (
-        <nav className="bg-primary text-text-primary p-4 flex justify-between items-center shadow-md">
-            <div className="font-heading text-2xl">DarkCart</div>
-            <div>
-                <button className="bg-secondary hover:bg-accent text-white px-4 py-2 rounded-lg transition">
-                    Cart
-                </button>
-            </div>
-        </nav>
-    );
+  const { cartItems } = useContext(CartContext);
+  const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <nav className="bg-amber-900 text-taupe px-6 py-4 flex justify-between items-center text-rose-200">
+      <Link to="/">
+        <div className="relative flex flex-col items-center leading-none">
+          <span className="absolute text-7xl font-playfair text-rose-300/50 select-none">
+            &amp;
+          </span>
+          <span className="relative font-playfair text-2xl text-rose-200 tracking-wide -ml-12">
+            Olive
+          </span>
+          <span className="relative font-playfair text-2xl text-rose-200 tracking-wide ml-12">
+            Oak
+          </span>
+        </div>
+      </Link>
+
+      <Link to="/cart">
+        <div className="relative hover:text-rose-300 cursor-pointer transition">
+          <ShoppingCartIcon className="w-8 h-8" />
+          {totalQuantity > 0 && (
+            <span className="absolute -top-2 -right-2 bg-red-300 text-amber-900 rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold transition-transform">
+              {totalQuantity}
+            </span>
+          )}
+        </div>
+      </Link>
+    </nav>
+  );
 }

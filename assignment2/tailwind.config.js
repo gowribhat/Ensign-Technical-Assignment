ba2d048124a5fd1bfec2dc65d274a82da9e2ed0d
Gowri Bhat <97337263+gowribhat@users.noreply.github.com>
2025-09-15 18:06:04 +0800

    Clean up config

--- assignment2/tailwind.config.js
+++ assignment2/tailwind.config.js
@@ -1,27 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#9D735C", // navbar/buttons
-                secondary: "#DCC5B1", // accents/buttons
-                accent: "#A7C7A9", // hover highlights
-                bg: "#E1C6AB", // page background
-                card: "#E0BFA2", // product cards
-                danger: "#EF4444", // remove buttons
-                "text-primary": "#000000",
-                "text-secondary": "#7A7A7A",
-            },
-            fontFamily: {
-                heading: ["Playfair Display", "serif"],
-                body: ["Lora", "serif"],
-            },
-            borderRadius: {
-                md: "0.5rem",
-                lg: "0.75rem",
-            },
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7777",
+        bg: "#E1C6AB",
+        "text-primary": "#000000",
+      },
+      fontFamily: {
+        heading: ["Playfair Display", "serif"],
+        body: ["Lora", "serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };

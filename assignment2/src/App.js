e895c46ef950eae9c1a07a1c6210e2f5c334fc8a
Gowri Bhat <97337263+gowribhat@users.noreply.github.com>
2025-09-15 16:26:17 +0800

    Fix deployment

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
 import { Toaster } from "react-hot-toast";
 import CartPage from "./pages/CartPage";
@@ -9,21 +9,19 @@ import ProductDetailPage from "./pages/ProductDetailsPage";
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <header className="h-fit flex-none">
-            <Navbar />
-          </header>
-          <main className="flex-1 flex items-center justify-center">
-            <Toaster position="bottom-left" toastOptions={{ duration: 1500 }} />
-            <Routes>
-              <Route path="/" element={<ProductsPage />} />
-              <Route path="/products/:id" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
+      <div className="min-h-screen flex flex-col">
+        <header className="h-fit flex-none">
+          <Navbar />
+        </header>
+        <main className="flex-1 flex items-center justify-center">
+          <Toaster position="bottom-left" toastOptions={{ duration: 1500 }} />
+          <Routes>
+            <Route path="/" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </main>
+      </div>
     </CartProvider>
   );
 }
